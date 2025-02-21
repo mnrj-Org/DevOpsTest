52a851b2e82917287dbbd3d3f4497b8fcfeeaa0e
Manraj Patel <mnrj.patel@gmail.com>
2025-02-21 10:23:58 +0530

    feature-1 added to program.cs

--- DevOpsTest.AppHost/Program.cs
+++ DevOpsTest.AppHost/Program.cs
@@ -1,5 +1,7 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
+// feature-1 implementation
+
 var apiService = builder.AddProject<Projects.DevOpsTest_ApiService>("apiservice");
 
 builder.AddProject<Projects.DevOpsTest_Web>("webfrontend")
