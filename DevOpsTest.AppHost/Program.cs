988e1d2e89ee6d350b18baa1b56ba5fa8d5f6fb9
Manraj Patel <mnrj.patel@gmail.com>
2025-03-18 20:05:55 +0530

    master changes

--- DevOpsTest.AppHost/Program.cs
+++ DevOpsTest.AppHost/Program.cs
@@ -1,7 +1,6 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
-// feature-2 implementation
-// fix bug-1 implementation
+// master changes
 
 var apiService = builder.AddProject<Projects.DevOpsTest_ApiService>("apiservice");
 
